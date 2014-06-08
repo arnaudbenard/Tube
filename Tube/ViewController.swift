69c4e8968620c643a583d08e7c57d5d786ffa06e
Arnaud Benard <arnaudbenard13@gmail.com>
2014-06-08 22:53:55 +0100

    display tube list

--- Tube/ViewController.swift
+++ Tube/ViewController.swift
@@ -11,8 +11,9 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
     
     var tableView: UITableView!
-    var tableViewData = ["aaa", "bbb", "ccc"]
-    
+    var tableViewData = []
+    var data: NSMutableData = NSMutableData()
+
     
     init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
@@ -29,6 +30,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         self.tableView.dataSource = self
         
         self.view.addSubview(self.tableView)
+        fetchTubeData()
+        
     }
 
     
@@ -39,11 +42,49 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     
     func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
-        let myNewCell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier("myCell", forIndexPath: indexPath) as UITableViewCell
+        let myNewCell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
         
-        myNewCell.text = tableViewData[indexPath.row]
+
+//        let myNewCell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier("myCell", forIndexPath: indexPath) as UITableViewCell
+        println(tableViewData)
+
+        myNewCell.text = tableViewData[indexPath.row] as String
         
         return myNewCell
+    }
+    
+    func fetchTubeData(){
+    
+        var urlPath = "http://localhost:4567/status"
+        var url: NSURL = NSURL(string: urlPath)
+        var request: NSURLRequest = NSURLRequest(URL: url)
+        var connection: NSURLConnection = NSURLConnection(request: request, delegate: self, startImmediately: false)
+        
+        connection.start()
+    }
+    
+    func connection(didReceiveResponse: NSURLConnection!, didReceiveResponse response: NSURLResponse!) {
+        // Recieved a new request, clear out the data object
+        self.data = NSMutableData()
+    }
+    
+    func connection(connection: NSURLConnection!, didReceiveData data: NSData!) {
+        // Append the recieved chunk of data to our data object
+        self.data.appendData(data)
+    }
+    
+    func connectionDidFinishLoading(connection: NSURLConnection!) {
+        // Request complete, self.data should now hold the resulting info
+        // Convert the retrieved data in to an object through JSON deserialization
+        var err: NSError
+        
+        var jsonResult: NSDictionary = NSJSONSerialization.JSONObjectWithData(self.data, options:NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
+        
+        if jsonResult.count>0 && jsonResult["result"].count>0 {
+            var results: NSArray = jsonResult["result"] as NSArray
+            self.tableViewData = results
+            self.tableView.reloadData()
+        }
     }
     
 }
